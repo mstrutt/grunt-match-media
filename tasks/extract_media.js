5c6ea5634221886574b2a964bc6797367496f6c5
Michael Strutt <michael@mstrutt.co.uk>
2013-12-05 12:18:14 +0000

    Finished basic functionality and bumped version 0.0.1

--- tasks/extract_media.js
+++ tasks/extract_media.js
@@ -16,14 +16,13 @@ module.exports = function(grunt) {
 	grunt.registerMultiTask('extract_media', 'Grunt plugin to extract styles matching certain width conditions, and create separate stylesheets with them', function() {
 		// Merge task-specific and/or target-specific options with these defaults.
 		var options = this.options({
-			punctuation: '.',
-			separator: ', '
+			width: '960px'
 		});
 
 		// Iterate over all specified file groups.
 		this.files.forEach(function(f) {
 			// Concat specified files.
-			var src = f.src.filter(function(filepath) {
+			var css = f.src.filter(function(filepath) {
 				// Warn on and remove invalid source files (if nonull was set).
 				if (!grunt.file.exists(filepath)) {
 					grunt.log.warn('Source file "' + filepath + '" not found.');
@@ -34,13 +33,60 @@ module.exports = function(grunt) {
 			}).map(function(filepath) {
 				// Read file source.
 				return grunt.file.read(filepath);
-			}).join(grunt.util.normalizelf(options.separator));
+			}).join("\n");
 
-			// Handle options.
-			src += options.punctuation;
+			// Real inner workings
+
+			function extractRules (mediaBlock) {
+				// Commenting out the head and tail for the @media declaration
+				mediaBlock = mediaBlock.replace(/(@media[^{]*{)/gmi, "/* $1 */");
+				mediaBlock = mediaBlock.substring(0, mediaBlock.lastIndexOf('}')) + "/* } */";
+				return mediaBlock;
+			}
+
+			function extractConditions (query) {
+				var conditions = [];
+				while (query.indexOf('(') > -1) {
+					var start = query.lastIndexOf('(');
+					conditions.push( query.substring( start+1, query.lastIndexOf(')') ) );
+					query = query.substring(0, start);
+				}
+				return conditions;
+			}
+
+			function evalMedia (width, query) {
+				var conditions = extractConditions(query),
+					match = true;
+
+				width = parseInt(width, 10);
+
+				while (conditions.length && match) {
+					var cond = conditions.pop().replace(' ', '').split(':'),
+						mWidth = parseInt(cond[1], 10);
+					if (cond[0] === 'min-width')
+						match = (width >= mWidth);
+					else
+						match = (width <= mWidth);
+				}
+
+				return match;
+			}
+
+			function getMediaBlocks (styles) {
+				return styles.match(/(@media[^{]*{(?:(?!}\s*})[\s\S])*}[\s\S]*?})/gmi);
+			}
+
+			var mediaBlocks = getMediaBlocks(css),
+				newMedia = [];
+
+			if (mediaBlocks)
+				mediaBlocks.forEach(function (mediaBlock) {
+					if (evalMedia(options.width, mediaBlock))
+						newMedia.push(extractRules(mediaBlock));
+				});
 
 			// Write the destination file.
-			grunt.file.write(f.dest, src);
+			grunt.file.write(f.dest, newMedia.join("\n\n"));
 
 			// Print a success message.
 			grunt.log.writeln('File "' + f.dest + '" created.');
