8a516bbc7c2932b6622a11abe200e9d3a435bd6d
Michael Strutt <michael@mstrutt.co.uk>
2013-12-05 12:16:06 +0000

    updating tests to match my project

--- test/extract_media_test.js
+++ test/extract_media_test.js
@@ -32,6 +32,7 @@ exports.extract_media = {
 
     var actual = grunt.file.read('tmp/default_options');
     var expected = grunt.file.read('test/expected/default_options');
+
     test.equal(actual, expected, 'should describe what the default behavior is.');
 
     test.done();
@@ -41,6 +42,7 @@ exports.extract_media = {
 
     var actual = grunt.file.read('tmp/custom_options');
     var expected = grunt.file.read('test/expected/custom_options');
+
     test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
 
     test.done();
